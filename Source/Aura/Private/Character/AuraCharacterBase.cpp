77ce124e5d2f101369e6af1a007cd8e1029e0132
Ryan Flores <rryanflorres@gmail.com>
2026-09-16 22:54:43 -0700

    move base class into character folder

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,15 @@
+// Copyright Ryan Flores :)
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
